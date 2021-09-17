b404a7a102e7203ca314830619bf51a20442b802
Piresssss <faelpiressilva@hotmail.com>
2021-09-17 20:43:43 -0300

    DTO

--- backend/src/main/java/com/rpires/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/rpires/dscatalog/services/CategoryService.java
@@ -1,10 +1,14 @@
 package com.rpires.dscatalog.services;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rpires.dscatalog.dto.CategoryDTO;
 import com.rpires.dscatalog.entities.Category;
 import com.rpires.dscatalog.repositories.CategoryRepository;
 
@@ -14,8 +18,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
-	
 }
