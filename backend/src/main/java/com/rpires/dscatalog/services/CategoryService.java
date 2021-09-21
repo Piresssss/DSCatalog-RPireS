e3c005307e839630231c85ff6095c1bfa4a5ea5d
Piresssss <faelpiressilva@hotmail.com>
2021-09-21 19:43:06 -0300

    Update category, method PUT

--- backend/src/main/java/com/rpires/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/rpires/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,14 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rpires.dscatalog.dto.CategoryDTO;
 import com.rpires.dscatalog.entities.Category;
 import com.rpires.dscatalog.repositories.CategoryRepository;
-import com.rpires.dscatalog.services.exceptions.EntityNotFoundException;
+import com.rpires.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
@@ -28,7 +30,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
@@ -39,4 +41,18 @@ public class CategoryService {
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
 }
